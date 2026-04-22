d535ce40a2cab0b3b0af7f7d966435b8b9e11d40
malak-immessaoudene <malak-ikram.immessaoudene@ensia.edu.dz>
2026-04-22 22:55:15 +0100

    Refactor Hospital class and add Node class

--- ambulance-dispatch/src/core/hospital.py
+++ ambulance-dispatch/src/core/hospital.py
@@ -1,6 +1,16 @@
-from core.node import Node
+import math
 
-class Hospital(Node):
-    def __init__(self, node_id, x, y, capacity=10):
-        super().__init__(node_id, x, y, "hospital")
-        self.capacity = capacity
+class Node:
+    def __init__(self, node_id, x, y, node_type="normal"):
+        self.id = node_id
+        self.x = x
+        self.y = y
+        self.type = node_type
+
+    def position(self):
+        return (self.x, self.y)
+
+    def euclidean_distance(self, other_node):
+        x1, y1 = self.position()
+        x2, y2 = other_node.position()
+        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
