3dc8976d3eadcc982387ea8c3ae9e242032c2d54
malak-immessaoudene <malak-ikram.immessaoudene@ensia.edu.dz>
2026-04-22 22:50:43 +0100

    Update ambulance.py

--- ambulance-dispatch/src/core/ambulance.py
+++ ambulance-dispatch/src/core/ambulance.py
@@ -1,23 +1,14 @@
 class Ambulance:
-    def __init__(self, amb_id, start_node):
-        self.id = amb_id
-        self.current_node = start_node
-        self.state = "idle"  # idle / busy
-        self.target = None
-        self.path = []
+    def __init__(self, ambulance_id, current_node):
+        self.id = ambulance_id
+        self.current_node = current_node
+        self.available = True
+        self.destination = None
 
-    def assign(self, emergency, hospital):
-        self.state = "busy"
-        self.target = emergency
-        self.hospital = hospital
+    def assign(self, emergency_node):
+        self.available = False
+        self.destination = emergency_node
 
-    def move_to(self, node):
-        self.current_node = node
-
-    def complete_task(self):
-        self.state = "idle"
-        self.target = None
-        self.path = []
-
-    def __repr__(self):
-        return f"Ambulance({self.id}, state={self.state}, at={self.current_node})"
+    def release(self):
+        self.available = True
+        self.destination = None
