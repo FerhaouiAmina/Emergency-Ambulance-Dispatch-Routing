2a21eba5ff122a16bccae953bc7895911afc14f4
malak-immessaoudene <malak-ikram.immessaoudene@ensia.edu.dz>
2026-05-10 23:06:01 +0100

    Refactor traffic multipliers to remove peak time

--- ambulance-dispatch/src/traffic/traffic_model.py
+++ ambulance-dispatch/src/traffic/traffic_model.py
@@ -1,26 +1,27 @@
 import json
 
+
 class TrafficModel:
     def __init__(self, data_file=None):
         self.time = "normal"
-
+        
         if data_file:
             with open(data_file, 'r') as f:
                 data = json.load(f)
             self.multipliers = data["congestion_multipliers"]
         else:
             self.multipliers = {
-                "highway": {"rush": 3.0, "peak": 3.2, "normal": 1.0, "night": 0.5},
-                "main": {"rush": 2.5, "peak": 3.8, "normal": 1.0, "night": 0.5},
-                "secondary": {"rush": 2.0, "peak": 2.5, "normal": 1.0, "night": 0.5},
-                "residential": {"rush": 1.5, "peak": 1.8, "normal": 1.0, "night": 0.7}
+                "highway": {"rush": 3.0, "normal": 1.0, "night": 0.5},
+                "main": {"rush": 2.5, "normal": 1.0, "night": 0.5},
+                "secondary": {"rush": 2.0, "normal": 1.0, "night": 0.5},
+                "residential": {"rush": 1.5, "normal": 1.0, "night": 0.5}
             }
-
+    
     def set_time(self, time):
         self.time = time
-
+    
     def get_multiplier(self, road_type):
         return self.multipliers[road_type][self.time]
-
+    
     def cost(self, base_cost, road_type):
         return base_cost * self.get_multiplier(road_type)
